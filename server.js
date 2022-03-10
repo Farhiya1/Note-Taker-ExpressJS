dc0025b8afbc8a0d565ed1bc853b23781e8512ce
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-10 15:49:25 +0000

    Added requirments needed to server.js file

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// Requirements
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/store");
+const path = require("path");
+const apiRouter = require("./routes/apiRoutes");
+const htmlRouter = require("./routes/htmlRoutes");
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
