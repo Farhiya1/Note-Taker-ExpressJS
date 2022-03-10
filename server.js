de2eb00f9ef5d5352ffd9aec313cc680087bcbb3
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-10 15:47:55 +0000

    Initialising App and assigning port variable

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-// Requirements
-const express = require("express");
-const fs = require("fs");
-const notes = require("./db/store");
-const path = require("path");
-const apiRouter = require("./routes/apiRoutes");
-const htmlRouter = require("./routes/htmlRoutes");
+// Initialise the app and assign PORT variable
+const app = express();
+const PORT = process.env.PORT || 3001;
+
