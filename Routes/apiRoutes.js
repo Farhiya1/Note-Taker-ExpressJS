658c7fd587c0f7ec581429ae440cc8078b34fcb9
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-10 16:10:50 +0000

    api notes response with notes from json file

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
 const res = require("express/lib/response");
 const store = require("../db/store");
+
+// /api/notes -> response with all notes from JSON file
+router.get("/api/notes", (request, response) => {
+  store
+    .getNotes()
+    .then((notes) => {
+      return response.json(notes);
+    })
+    .catch((error) => response.status(500).json(error));
+});
+
