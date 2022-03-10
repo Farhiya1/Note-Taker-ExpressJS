98098737debbf4b9a271d56033fa6d6ac97ea3c6
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-10 15:48:17 +0000

    Added middleware needed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Initialise the app and assign PORT variable
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("/public"));
