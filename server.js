4f3c6e0f7b3fe0f2fc5b543ad7ace5116ec016cf
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-10 16:13:13 +0000

    added middleware to server.js file

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("/public"));
 
+app.use("/api", apiRouter);
+app.use("/", htmlRouter);
+app.use("/api/notes", notes);
+
 // Start the app on the given port
 app.listen(PORT, () => {
   console.log("App listening.....");
