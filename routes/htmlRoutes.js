4eaf0fb3fe3c727ed156db5a60ccb1d5ce7eb369
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-11 20:00:37 +0000

    returned function syntax to what it was before

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ const path = require("path");
 const router = require("express").Router();
 
 // /notes -> give back notes.html
-router.get("/notes", function (request, response) {
+router.get("/notes", (request, response) => {
   response.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 // /* -> give back index.html
-router.get("*", function (request, response) {
+router.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
